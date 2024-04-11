103563b93357590ba360e16ae45d1c494f672ea7
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-04-11 15:57:43 -0400

    Agregadas validaciones al endpoint POST que creal el token para evitar que creara el token sin validar que la contraseña sea correcta primero

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,18 +1,25 @@
 import jwt from "jsonwebtoken";
 import {NextResponse} from "next/server";
 import {prisma} from "@/libs/prisma";
+import bcrypt from "bcrypt";
 
 export async function POST(request) {
     try {
         const {email, password} = await request.json()
+        if(email == null || password == null) return NextResponse.json({"message": "Email or password is missing"}, {status: 401})
         const userRegistered = await prisma.user.findUnique({
             where: {
                 email: email
             }
         })
-        const secret = process.env.JWT_SECRET
-        const token = jwt.sign({email: userRegistered.email}, secret, {expiresIn: "20m"})
-        return NextResponse.json({"token": token, "user": userRegistered})
+        if(userRegistered == null) return NextResponse.json({"message": "User not found"}, {status: 404})
+        const correctPassword = await bcrypt.compare(password, userRegistered.password)
+        if(correctPassword){
+                const secret = process.env.JWT_SECRET
+                const token = jwt.sign({email: userRegistered.email}, secret, {expiresIn: "20m"})
+                return NextResponse.json({"token": token, "user": userRegistered})
+        }
+        return NextResponse.json({"message": "password doesn't match"}, {status: 401})
     } catch (error) {
         return NextResponse.json(error.message)
     }
