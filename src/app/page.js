17bf4dba490603b0e51d7114ebb6c13a03eae90a
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-04-14 23:47:05 -0400

    Add get request of users in userSlice with createAsyncThunk

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,22 @@
+"use client"
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUsers } from "@/redux/userSlice";
+
+
 
 export default function Home() {
+  
+  const dispatch = useDispatch()
+  const users = useSelector(state => state.users)
+
+  useEffect(() =>{
+    dispatch(getUsers())
+  }, [])
+
+  console.log(users)
+
   return (
       <>
         <h1 className="text-2xl text-slate-600 uppercase">Hola Mundo Magnifico</h1>
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,8 +1,19 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
+export const getUsers = createAsyncThunk('get/users', async () =>{
+    try {
+        const resp = await fetch("/api/users")
+        const data = await resp.json()
+        // console.log(data)
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 const initialState = {
-    users: [],
+    data: [],
     loading: false,
     error: null
 }
@@ -12,7 +23,20 @@ export const userSlice = createSlice({
     initialState,
     reducers:{},
     extraReducers:(builder) => {
-        
+        builder
+            .addCase(getUsers.fulfilled, (state, action) => {
+                state.data = action.payload
+                state.loading = false
+                state.error = null
+            })
+            .addCase(getUsers.pending, (state, action) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(getUsers.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
     }
 })
 
