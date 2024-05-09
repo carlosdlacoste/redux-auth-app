a509d4021dae06da9a34ef4ec6525e5f3a98793e
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-09 18:09:37 -0400

    Add controlled inputs in login page and fix error in login request in authSlice

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,28 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
+import { login } from "@/redux/authSlice";
 
 const Login = () =>{
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
+    const router = useRouter()
+
+    const handleLogIn = async (event) => {
+        event.preventDefault()
+        console.log('paso 1')
+        const response = await dispatch(login(email, password))
+        console.log(response)
+        if(response.type === login.fulfilled.type) {
+            console.log('paso 3')
+            router.push('/')
+        }
+
+    }
 
     return(
         <>
@@ -14,7 +36,7 @@ const Login = () =>{
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
                             <div className="mt-2">
-                                <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset p-2 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"/>
+                                <input id="email" name="email" onChange={(event) => setEmail(event.target.value)} value={email || ''} type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset p-2 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"/>
                             </div>
                         </div>
 
@@ -26,12 +48,12 @@ const Login = () =>{
                                 </div>
                             </div>
                             <div className="mt-2">
-                                <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm p-2 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"/>
+                                <input id="password" name="password" onChange={(event) => setPassword(event.target.value)} value={password || ''} type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm p-2 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"/>
                             </div>
                         </div>
 
                         <div>
-                            <button type="submit" className="flex w-full justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                            <button onClick={(event) => handleLogIn(event)} className="flex w-full justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                         </div>
                     </form>
 
--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,13 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const login = createAsyncThunk("auth/user", async (email, password) => {
-    const state = getState()
     try {
         const resp = await fetch('/api/auth', {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + state.auth.token
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 email: email,
@@ -16,7 +14,7 @@ export const login = createAsyncThunk("auth/user", async (email, password) => {
         })
         const data = await resp.json()
         console.log(data)
-        // return data
+        return data
     } catch (error) {
         console.log(error);
     }
@@ -37,22 +35,22 @@ export const authSlice = createSlice({
         // }
     },
     extraReducers: (builder) => {
-    builder
-        .addCase(login.fulfilled, (state, action) => {
-            const {token, user} = action.payload
-            state.token = token
-            state.userLoggedIn = user
-            state.loading = false
-            state.error = null
-        })
-        .addCase(login.pending, (state, action) => {
-            state.loading = true
-            state.error = null
-        })
-        .addCase(login.rejected, (state, action) => {
-            state.loading = false
-            state.error = action.payload
-        })
+        builder
+            .addCase(login.fulfilled, (state, action) => {
+                const {token, user} = action.payload
+                state.token = token
+                state.userLoggedIn = user
+                state.loading = false
+                state.error = null
+            })
+            .addCase(login.pending, (state, action) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
     }
 })
 
