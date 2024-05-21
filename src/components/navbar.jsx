238f2d5005a5c10009f2a82f8569d63d9de66152
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-21 18:45:12 -0400

    Create log out button and syncronized with my token and create a removeUserFromStorage method in authSlice reducers section to delete the token and user info

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,11 @@
+"use client"
 import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUserFromStorage } from "@/redux/authSlice";
 
 const NavBar = () =>{
+    const dispatch = useDispatch()
+    const token = useSelector(state => state.auth.token)
     return(
         <>
             <nav className="bg-gray-950">
@@ -10,12 +15,21 @@ const NavBar = () =>{
                             <h2 className="text-white text-2xl">Auth System</h2>
                         </Link>
                         <div className="space-x-4">
-                            <Link href="/login">
-                                <button className="bg-green-700 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm">Log In</button>
-                            </Link>
-                            <Link href="/signup">
-                                <button className="bg-yellow-600 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm">Sign up</button>
-                            </Link>
+                            {!token ?
+                            <>
+                                <Link href="/login">
+                                    <button className="bg-green-700 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm">Log In</button>
+                                </Link>
+                                <Link href="/signup">
+                                    <button className="bg-yellow-600 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm">Sign up</button>
+                                </Link>
+                            </>
+                            :
+                            <>
+                                    <button className="bg-red-700 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm" onClick={() => dispatch(removeUserFromStorage())}>Log Out</button>
+                            </>
+
+                            }
                         </div>
                     </div>
                 </div>
