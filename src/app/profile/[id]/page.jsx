4774bf12652cfc6fab8c577ab9c9e3c6619dde5f
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-24 19:17:32 -0400

    Create preliminar profile component, just simple design

---
+++ src/app/profile/[id]/page.jsx
@@ -0,0 +1,15 @@
+const Profile = ({params}) => {
+    return(
+        <>
+            <div>
+                <h3>Bienvenido!</h3>
+            </div>
+            <div>
+                <p>ID:</p>
+                <p>Full Name:</p>
+                <p>Email: </p>
+            </div>
+        </>
+    )
+}
+export default Profile
