99946a903009297cbc85a7a2768f26d011176662
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-28 17:00:28 -0400

    Navigate to home page after logout action

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,16 @@
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { removeUserFromStorage } from "@/redux/authSlice";
+import { useRouter } from "next/navigation";
 
 const NavBar = () =>{
     const dispatch = useDispatch()
     const token = useSelector(state => state.auth.token)
+    const router = useRouter()
+    const handleLogOut = () =>{
+        dispatch(removeUserFromStorage())
+        router.push("/")
+    }
     return(
         <>
             <nav className="bg-gray-950">
@@ -26,7 +32,7 @@ const NavBar = () =>{
                             </>
                             :
                             <>
-                                    <button className="bg-red-700 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm" onClick={() => dispatch(removeUserFromStorage())}>Log Out</button>
+                                    <button className="bg-red-700 hover:bg-slate-300 py-2 px-4 rounded-md border-2 border-slate-50 text-sm" onClick={handleLogOut}>Log Out</button>
                             </>
 
                             }
