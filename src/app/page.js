7b98aed26ae70fefe1c0fd39cf058db968fb3a33
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-05-28 15:34:30 -0400

    Add console.log of userLoggedIn to verify the data and add a preliminar button to test the profile page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUsers } from "@/redux/userSlice";
+import {Profile} from "./profile/[id]/page";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 
 
@@ -10,16 +13,30 @@ export default function Home() {
 
   const dispatch = useDispatch()
   // const users = useSelector(state => state.users)
-  // const token = useSelector(state => state.auth.token)
-
-  useEffect(() =>{
-    dispatch(getUsers())
-  }, [])
-
+  const token = useSelector(state => state.auth.token)
+  const user = useSelector(state => state.auth.userLoggedIn)
+  const router = useRouter()
+  // useEffect(() =>{
+  //   dispatch(getUsers())
+  // }, [])
+  console.log(user)
 
   return (
       <>
-        <h1 className="text-2xl text-slate-600 uppercase">Hola Mundo Magnifico</h1>
+      <h1 className="text-2xl text-slate-600 uppercase">Hola Mundo Magnifico</h1>
+      <button onClick={() => router.push(`/profile/${user?.id}`)}>View Profile</button>
+        {/* {token ?
+        <>
+        <Link href={`/profile/${params.id}`}>
+          <Profile/>
+        </Link>
+        </>
+        :
+        <>
+          <h1 className="text-2xl text-slate-600 uppercase">Hola Mundo Magnifico</h1>
+        </>
+        } */}
+
       </>
   );
 }
