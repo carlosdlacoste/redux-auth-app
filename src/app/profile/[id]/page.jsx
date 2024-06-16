bcd90634240c06c9c97ee7f7ed7d0476e8e811c3
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-06-16 04:01:44 -0400

    Add image and styles to profile page

--- src/app/profile/[id]/page.jsx
+++ src/app/profile/[id]/page.jsx
@@ -14,13 +14,18 @@ const Profile = ({params}) => {
 
     return(
         <>
-            <div>
-                <h3>Bienvenido! {user.fullName}</h3>
-            </div>
-            <div>
-                <p>ID: {user.id}</p>
-                <p>Full Name: {user.fullName}</p>
-                <p>Email: {user.email}</p>
+            <div className="flex h-screen flex-col justify-center items-center px-6 py-12 lg:px-8">
+                <div className="w-48 h-48 mb-3">
+                    <img className="object-cover object-top h-full w-full rounded-full" src="https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Foto perfil de prueba" />
+                </div>
+                <div>
+                    <h3>Bienvenido! {user.fullName}</h3>
+                </div>
+                <div>
+                    <p>ID: {user.id}</p>
+                    <p>Full Name: {user.fullName}</p>
+                    <p>Email: {user.email}</p>
+                </div>
             </div>
         </>
     )
