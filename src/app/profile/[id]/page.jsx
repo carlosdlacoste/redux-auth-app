c644f97ed3caee590a54dc7c6aa81069a5750cb9
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-06-17 18:25:20 -0400

    Add styles to the profile card and table

--- src/app/profile/[id]/page.jsx
+++ src/app/profile/[id]/page.jsx
@@ -27,59 +27,32 @@ const Profile = ({params}) => {
                     <p><i className="fa-solid fa-envelope" /> : {user.email}</p>
                 </div> */}
                 <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex flex-col items-center pb-10">
-                        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Foto perfil de prueba" />
+                    <div className="flex flex-col items-center">
+                        <img className="w-48 h-48 my-3 rounded-full shadow-lg" src="https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Foto perfil de prueba" />
                         {/* <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5> */}
                         <table className="table-fixed w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <thead className="text-xs text-white uppercase bg-gray-900 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th className="text-center px-6 py-3" colSpan="100%">
                                         Profile details
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Apple MacBook Pro 17"
+                                <tr className="bg-gray-700 border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" className="text-center px-6 py-4 font-medium text-white whitespace-nowrap dark:text-gray-900" colSpan="100%">
+                                        <i className="fa-solid fa-id-card" /> : {user.id}
                                     </th>
-                                    <td className="px-6 py-4">
-                                        Silver
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        Laptop
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        $2999
-                                    </td>
                                 </tr>
-                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Microsoft Surface Pro
+                                <tr className="bg-gray-700 border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" className="text-center px-6 py-4 font-medium text-white whitespace-nowrap dark:text-gray-900" colSpan="100%">
+                                        <i className="fa-solid fa-signature" /> : {user.fullName}
                                     </th>
-                                    <td className="px-6 py-4">
-                                        White
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        Laptop PC
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        $1999
-                                    </td>
                                 </tr>
-                                <tr className="bg-white dark:bg-gray-800">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Magic Mouse 2
+                                <tr className="bg-gray-700 dark:bg-gray-800">
+                                    <th scope="row" className="text-center px-6 py-4 font-medium text-white whitespace-nowrap dark:text-gray-900" colSpan="100%">
+                                        <i className="fa-solid fa-envelope" /> : {user.email}
                                     </th>
-                                    <td className="px-6 py-4">
-                                        Black
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        Accessories
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        $99
-                                    </td>
                                 </tr>
                             </tbody>
                         </table>
