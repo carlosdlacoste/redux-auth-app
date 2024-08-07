9489b70c8c1122a528312f964b0fa3af0eaa56bd
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-08-07 12:30:08 -0400

    Add funcionalities descriptions in home page

--- src/app/page.js
+++ src/app/page.js
@@ -31,17 +31,20 @@ export default function Home() {
                 You can find the registration and login forms in the “Sign Up” and “Log In” sections respectively
                 in the NavBar at the top of your screen &#x261D;.
               </p>
-            <div className="flex flex-col h-screen justify-center items-center px-6 py-12">
-              <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Password requirements:</h2>
+            <div className="flex flex-col h-screen justify-center items-center px-6 py-6">
+              <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Discover our functionalities:</h2>
               <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
                   <li>
-                      At least 10 characters (and up to 100 characters)
+                      In login page you can clear the whole password if you made a mistake while you were typing it.
                   </li>
                   <li>
-                      At least one lowercase character
+                      You can use visibility icon inside password's input to see what you are typing.
                   </li>
                   <li>
-                      Inclusion of at least one special character, e.g., ! @ # ?
+                      You can see a validation after you send your credentials in the login form. If credentials are correct, you will see a welcome message. In any other case, you will see an error message.
+                  </li>
+                  <li>
+                      When you are authenticated, you can see your profile info. In any other case, you will see an error alert due to you have no permission to see private views.
                   </li>
               </ul>
             </div>
